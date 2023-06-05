01dc0fa92a90815b0f6861abd5bc53e8b16cfc7c
Kaiqbal29 <iqbalfaris@students.amikom.ac.id>
2023-06-05 22:16:44 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static prak8.barang;
+
+
+namespace prak8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            barang orang01 = new barang();
+            barang orang02 = new barang("Cocacola", 9000);
+            dataBrg data_barang1 = new dataBrg("Sprite", 8000);
+
+            orang01.InfoBarang();
+            orang02.InfoBarang();
+
+            data_barang1.InfoBarang();
+            data_barang1.InfoBarang("Minuman", "CC0122");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ barang.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace prak8
+{
+    public class barang
+    {
+        public string nama_barang { get; set; }
+        
+        public int harga { get; set; }
+
+
+        public barang()
+        {
+            this.nama_barang = "[belum diatur]";
+     
+            this.harga = 0;
+        }
+
+        public barang(string nama_barang, int harga)
+        {
+            this.nama_barang=nama_barang;
+            this.harga = 50000;
+        }
+        public void InfoBarang()
+        {
+            Console.WriteLine("Nama produk {0}, dan harganya Rp. {1}\n", nama_barang, harga);
+        }
+
+        public class dataBrg : barang
+        {
+            public string IDbarang { get; set; }
+            public string jenis { get; set; }
+            public dataBrg()
+            {
+                nama_barang = "[belum diatur]";
+                harga = 0;
+            }
+            public dataBrg(string Nama_baramg, int harga)
+            {
+                this.nama_barang = nama_barang;
+                this.harga = harga;
+            }
+
+            public void InfoBarang(string IDbarang, string jenis)
+            {
+                this.nama_barang = nama_barang;
+                this.IDbarang = IDbarang;
+                this.jenis = jenis;
+
+                Console.WriteLine("{0} adalah produk berjenis {1}, dengan ID barang {2}",this.nama_barang, this.IDbarang, this.jenis);
+            }
+        }
+
+    }
+}
